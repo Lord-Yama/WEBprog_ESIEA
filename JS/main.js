5f37562a43b5a0233f165c97585262885054e49c
Lord-Yama <57375119+Lord-Yama@users.noreply.github.com>
2021-05-09 23:05:03 +0200

    avancement conséquent

--- JS/main.js
+++ JS/main.js
@@ -6,7 +6,7 @@ function plusSlides(n){
   if (n < 0){
     showSlides(slideIndex -= 1);
   } else {
-   showSlides(slideIndex += 1); 
+    showSlides(slideIndex += 1); 
   }
   if (n == -1){
     myTimer = setInterval(function(){plusSlides(n + 2)}, 6000);
@@ -22,10 +22,10 @@ function showSlides(n){
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+    slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
